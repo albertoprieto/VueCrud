8099d80c4cb46eb529d91a9851975c44ef8ed7de
dev jap <alberto.preto.r@gmail.com>
2024-12-21 17:46:26 -0600

    FIX::grales

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VueCrud/', // Asegúrate de que esta ruta sea correcta para GitHub Pages
+  base: '/VueCrud/',
   plugins: [
     vue(),
   ],
