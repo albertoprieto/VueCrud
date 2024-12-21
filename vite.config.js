2d2b28cca51423fbf70aaa4a35a5386abe9377bc
JoseAlbertoPrieto <alberto.prieto.r@gmail.com>
2024-12-21 17:41:30 -0600

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/adminstore/',
+  base: '/VueCrud/', // Asegúrate de que esta ruta sea correcta para GitHub Pages
   plugins: [
     vue(),
   ],
