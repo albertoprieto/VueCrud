94c30cdfa01baef07d556a7804f7fd679f2ee87f
x <alberto.prieto.r@gmail.com>
2025-06-01 11:41:07 -0600

    Updates

--- assets/index-B8bPARDm.js
+++ assets/index-B8bPARDm.js
@@ -1,4 +1,4 @@
-import{N as h,aq as m,ar as u,e as i,c as r,ac as s,ab as c,f as l,ad as p,h as t,t as g}from"./index-dE5x1ZbV.js";var v=function(o){var e=o.dt;return`
+import{N as h,aq as m,ar as u,e as i,c as r,ac as s,ab as c,f as l,ad as p,h as t,t as g}from"./index-DHsczmXD.js";var v=function(o){var e=o.dt;return`
 .p-chip {
     display: inline-flex;
     align-items: center;
