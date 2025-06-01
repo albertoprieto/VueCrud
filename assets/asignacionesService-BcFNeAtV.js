1f737a4b9cd1a2f68a68d4518e881807d398a520
x <alberto.prieto.r@gmail.com>
2025-06-01 16:13:37 -0600

    Updates

---
+++ assets/asignacionesService-BcFNeAtV.js
@@ -0,0 +1,1 @@
+import{d as s}from"./index-XKA0IK-S.js";const c=async()=>(await s.get("https://api.gpsubicacionapi.com/asignaciones-tecnicos")).data;export{c as g};
--- assets/asignacionesService-DoA71KrZ.js
+++
@@ -1,1 +0,0 @@
-import{d as s}from"./index-DKaPuLj0.js";const c=async()=>(await s.get("https://api.gpsubicacionapi.com/asignaciones-tecnicos")).data;export{c as g};
