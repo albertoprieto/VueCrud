58e9c3c3a5f2e4b8fc1b6b79880a2c07ba36ca11
APN <ja.prieto@mavi.mx>
2025-09-08 13:25:54 -0600

    FEAT::Documento de avances

--- delete_ventas.sql
+++ delete_ventas.sql
@@ -16,3 +16,12 @@ DELETE FROM reportes;
 DELETE FROM ventas;
 
 UPDATE imeis SET status = 'Disponible';
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE cotizaciones_enviadas;
+TRUNCATE detalle_venta;
+TRUNCATE movimientos_dinero;
+TRUNCATE reportes;
+TRUNCATE reportes_servicio;
+TRUNCATE cotizaciones;
+TRUNCATE ventas;
+SET FOREIGN_KEY_CHECKS = 1;
